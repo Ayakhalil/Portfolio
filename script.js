fff7f3e41a357fe6bb70c3631705d5950e944e63
Ayakhalil <ayahkhaleel@yahoo.com>
2022-06-16 18:27:40 +0300

    Intiail Commit

---
+++ script.js
@@ -0,0 +1,8 @@
+let menu = document.getElementById('menu');
+		let nav = document.querySelector('.Mainmenu')
+
+		menu.onclick = function(){
+			menu.classList.toggle('openmenu');
+
+			nav.classList.toggle('show')
+				}
